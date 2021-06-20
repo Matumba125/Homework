9f6ea35d7cbab28ed90363b910ed129213eccead
matumba 125 <nbenza5@gmail.com>
2021-06-20 15:42:16 +0300

    finished third homework

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -9,13 +9,19 @@ type AffairPropsType = {
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
-     props.deleteAffairCallback(props.affair._id);
+        props.deleteAffairCallback(props.affair._id);
     }
+    const priorityStyle = {
+        color:  (props.affair.priority === "high") ? "red" :
+                (props.affair.priority === "middle") ? "yellow" :
+                (props.affair.priority === "low") ? "orange" : "black"
+    }
+
 
     return (
         <div className={style.affair}>
             <div>{props.affair.name}</div>
-            <div className={style.priority}>{props.affair.priority}</div>
+            <div className={style.priority} style={priorityStyle}>{props.affair.priority}</div>
 
             <button onClick={deleteCallback} className={style.button}>X</button>
         </div>
